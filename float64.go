ccad6fca0d51c972e7e706c5fb257932cf02c50d
Dias Taufik <diastaufik@gmail.com>
2019-12-24 00:05:30 +0700

    initial commit

---
+++ float64.go
@@ -0,0 +1,12 @@
+package tdd
+
+import "fmt"
+
+func ParseFloat64(val interface{}) (float64, error) {
+	switch val.(type) {
+	case nil:
+		return 1, nil
+	default:
+		return -1, fmt.Errorf("unable to casting numver %v (type %T)", val, val)
+	}
+}
---
+++ float64_test.go
@@ -0,0 +1,28 @@
+package tdd_test
+
+import (
+	"encoding/json"
+	"github.com/mydisha/tdd"
+	"testing"
+)
+
+func TestParseFloat64(t *testing.T) {
+	t.Run("Nil input", func(t *testing.T) {
+		var i interface{}
+
+		convertNum, _ := tdd.ParseFloat64(i)
+
+		if convertNum != 1 {
+			t.Fatalf("return must be 1 instead of %v", convertNum)
+		}
+	})
+	t.Run("Json number", func(t *testing.T) {
+		var i interface{} = json.Number("20.4")
+
+		convertedNum, _ := tdd.ParseFloat64(i)
+
+		if convertedNum != 20.4 {
+			t.Fatalf("return must be 20.4 instead of %v", convertedNum)
+		}
+	})
+}
